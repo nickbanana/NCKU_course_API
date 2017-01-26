d41812f5ae0c8d2db46415ada6236238bd327edc
nickbanana <nick.chi@gmail.com>
2017-01-26 17:12:04 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express'); // dependency
+var app = express(); // add the object
+var router = express.Router();
+app.set('view engine', 'ejs'); //set the view engine to ejs
+
+// use res.render to load up an ejs view file
+
+app.get('/', function(req,res){
+    res.render('pages/index');
+});
+
+
+
+router.get('/test', function(req, res){
+    res.json({"message": "hello test"});
+});
+
+
+
+
+
+
+app.use('/api', router);
+app.listen(8080);
+console.log('8080 so magic');
